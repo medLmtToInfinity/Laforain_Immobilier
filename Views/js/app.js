8574ff76f5300e776bf537d82b946981658af55f
medLmtToInfinity <med.lmt012@gmail.com>
2023-03-14 12:00:54 -0700

    nav bar
    loading

---
+++ Views/js/app.js
@@ -0,0 +1,15 @@
+// const selects = document.querySelectorAll(".search-handler .select-handler");
+
+// selects.forEach((select) => {
+//   select.addEventListener("change", () => {
+//     const arrow = select.querySelector("i");
+//     // console.log(arrow.querySelector("i"));
+//     if (arrow.classList.contains("fa-arrow-down")) {
+//       arrow.classList.remove("fa-arrow-down");
+//       arrow.classList.add("fa-arrow-up");
+//     } else {
+//       arrow.classList.remove("fa-arrow-up");
+//       arrow.classList.add("fa-arrow-down");
+//     }
+//   });
+// });
