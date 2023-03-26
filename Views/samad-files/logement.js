d1c739400010eb7b686ea4dbaee93f71e171cb51
ABDESSAMAD AIT HAMOU <a.aithamou1046@uca.ac.ma>
2023-03-26 13:35:39 +0000

    I add infromation links

---
+++ Views/samad-files/logement.js
@@ -0,0 +1,19 @@
+
+
+
+const btns = document.querySelectorAll('.info-btn');
+const contents = document.querySelectorAll('.content');
+
+const activeState = (index)=>{
+   contents.forEach((content,i)=>{
+      if(i == index)
+         content.classList.remove('hidden-content');
+       else
+         content.classList.add('hidden-content');
+   })
+}
+
+btns.forEach((btn, index)=>{
+   btn.addEventListener('click',()=>activeState(index))
+})
+
