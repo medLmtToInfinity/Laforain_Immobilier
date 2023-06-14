71e01f69d9a85a7fc974a34b5a32373752fe3001
medlmt <med.lmt012@gmail.com>
2023-06-14 16:00:08 +0100

    last one

--- Views/dashboard/edit_post.php
+++ Views/dashboard/edit_post.php
@@ -6,11 +6,58 @@
 <?php
     include "dashboard-header-aside.php";
 
+    $type = $title = $price = $locat = $dscrption = $area =  $bathrooms = $bedrooms = $city = $state = $sell_rent = "";
 
-    if(isset($_POST['submit'])){
+    if(isset($_GET['editID'])){
+        $postId = $_GET['editID'];
+        $query = "SELECT * FROM posts WHERE id = $postId";
+        $stmt = $dbConnection->query($query);
+        if ($stmt !== false){
+            $result     = $stmt->fetch(PDO::FETCH_ASSOC);
+            //get the informations about the post from the `fiche_technique` table
+            $query = "SELECT * FROM fiche_technique WHERE id = ?";
+            $stmt = $dbConnection->prepare($query);
+            $stmt->execute([$result['info_id']]);
+            $infos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            //get the city of the post from the `cities` table
+            $query = "SELECT city_name FROM cities WHERE id = ?";
+            $stmt = $dbConnection->prepare($query);
+            $stmt->execute([$result['city_id']]);
+            $city = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $catId      = $result['cat_id'];
+            $title      = $result['title'];
+            $price      = $result['price'];
+            $locat      = $result['locat'];
+            $dscrption  = $result['dscrption'];
+            $area       = $infos['area'];
+            $bedrooms   = $infos['bedrooms'];
+            $bathrooms  = $infos['bathrooms'];
+            $state      = $result['stat'];
+            $sell_rent  = $result['rentOrSell'];
+
+            //get the type of the property
+            $query = "SELECT cat_name FROM categories WHERE id = ? LIMIT 1";
+            $stmt = $dbConnection->prepare($query);
+            $stmt->execute([$catId]);
+            
+            $type = $stmt->fetch(PDO::FETCH_COLUMN);
+            
+            //get images of the property
+            $query = "SELECT img_name FROM post_imgs WHERE post_id = ?";
+            $stmt = $dbConnection->prepare($query);
+            $stmt->execute([$postId]);
+
+            $images = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        }
+    }
+
+    if(isset($_POST['edit'])){
         // $countImg = count($_FILES['photos']['name']);
         // loop through the uploaded files
-        $type       = $_POST['select'];
+        $postId     = $_GET['editID'];
+        $type       = $_POST['select-type'];
         $title      = $_POST['title'];
         $price      = $_POST['price'];
         $locat      = $_POST['location'];
@@ -18,21 +65,16 @@
         $area       = $_POST['area'];
         $bedrooms       = $_POST['bedrooms'];
         $bathrooms       = $_POST['bathrooms'];
-        $city_id = $_POST['city'];
+        $city = $_POST['city'];
         $state       = $_POST['state'];
         $sell_rent       = $_POST['select_type'];
-
-
-        $stmt       = $dbConnection->prepare("INSERT INTO posts (cat_id, city_id, Price, title, locat, dscrption, stat, rentOrSell) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$type, $city_id, $price, $title, $locat, $dscrcption, $state, $sell_rent]);
+        $stmt       = $dbConnection->prepare("UPDATE posts SET cat_id = ?, city_id = ?, Price = ?, title = ?, locat = ?, dscrption = ?, stat = ?, rentOrSell = ? WHERE id = $postId");
+        $stmt->execute([$type, $city, $price, $title, $locat, $dscrcption, $state, $sell_rent]);
 
         //get the ID of the element we just added to use it in post_imgs table
-        $stmt1      = $dbConnection->query("SELECT id FROM posts ORDER BY id");
-        $data       = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-        // print_r($data);
-        $postId    = end($data)['id'];
-        //echo "<h1 style='color: red; font-size:40px'>".$postId . "</h1>";
-
+        // $stmt1      = $dbConnection->query("SELECT id FROM posts");
+        // $data       = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+        // $postId    = end($data)['id'];
         foreach ($_FILES['photos']['name'] as $i => $name) {
             // check if the file is uploaded successfully
 
@@ -44,33 +86,27 @@
                 move_uploaded_file($_FILES["photos"]["tmp_name"][$i], $targetFilePath);
                 $stmt = $dbConnection->prepare("INSERT INTO post_imgs (post_id, img_name) VALUES (?, ?)");
                 $stmt->execute([$postId, $name]);
-            
-
-
-                //BLOB
-                // $imageData = file_get_contents($_FILES["photos"]["tmp_name"]);
-                // echo $imageData;
-                // $stmt = $dbConnection->prepare("INSERT INTO post_imgs (post_id, img_data) VALUES (?, ?)");
-                // $stmt->execute([$postId, $imageData]);
             }
         }
+        sleep(10);
+        header("Location: view_posts.php");
     }
-
 ?>
 
 
     <div id="success-message" class="hidden">
-        <p>Success! the post has been added successfully.</p>
+        <p>Success! the post has been modified successfully.</p>
     </div>
 
     <form action="" method="POST" class="add-post-form" enctype="multipart/form-data">
-        <h1>Ajouter une annonce</h1>
+        <h1>Modifier une annonce</h1>
         
         <div class="add-post-flex">
         <div class="add-post-left">
-            <label for="type">Type :</label>
-            <select class="select-cat" name="select" required>
+            <label for="type">Type :<span class="red-star">*</span></label>
+            <select class="select-cat" name="select-type" required>
             <?php
+                //echo "<option value='". $catId ."'>" . $type .  "</option>";
                 $query = "SELECT * FROM categories";
                 $result = $dbConnection->query($query);
                 
@@ -85,28 +121,29 @@
 
             <!-- <input type="text" id="type" name="type" > -->
             <label for="price">Prix :<span class="red-star">*</span></label>
-            <input type="number" id="price" name="price" value="" required>
+            <input type="number" id="price" name="price" value="<?php echo $price;?>"  required>
             
             <label for="location">Emplacement :<span class="red-star">*</span></label>
-            <input type="text" id="location" name="location" value="" required>
+            <input type="text" id="location" name="location" value="<?php echo $locat;?>"  required>
             
             <label for="description">Description :</label>
-            <input type="text" id="description" name="description" rows="1" value="" ></textarea>
+            <input type="text" id="description" name="description" rows="1" value="<?php echo $dscrption;?>" ></textarea>
             
             <label for="bedrooms">Bedrooms :</label>
-            <input type="number" id="bedrooms" name="bedrooms" value="" >
+            <input type="number" id="bedrooms" name="bedrooms" value="<?php echo $bedrooms;?>" >
             
             <label for="bathrooms">Bathrooms:</label>
-            <input type="number" id="bathrooms" name="bathrooms" value="" >
+            <input type="number" id="bathrooms" name="bathrooms" value="<?php echo $bathrooms;?>" >
         </div>
         <div class="add-post-right">
 
             <label for="title">Title:<span class="red-star">*</span></label>
-            <input type="text" id="title" name="title" value="" required>
+            <input type="text" id="title" name="title" value="<?php echo $title;?>"  required>
 
             <label for="city">Ville:<span class="red-star">*</span></label>
-            <select class="select-cat" name="city" required>
+            <select class="select-cat" name="select-city" required>
             <?php
+                //echo "<option value='". $catId ."'>" . $type .  "</option>";
                 $query = "SELECT * FROM cities";
                 $result = $dbConnection->query($query);
                 
@@ -116,6 +153,7 @@
                         echo "<option value='". $city['id']."'>" . $city['city_name'] .  "</option>";
                 }
             ?>
+
             </select>
 
             <label for="state">State:<span class="red-star">*</span></label>
@@ -130,16 +168,15 @@
                 <option value='Sell'>Sell</option>;
                 <option value='RentSais'>Location saisonière</option>;
             </select>
-
             <label for="area">Superficie :</label>
-            <input type="number" id="area" name="area" value="" >
+            <input type="number" id="area" name="area" value="<?php echo $area;?>" >
             
             <label for="photos">Photos :<span class="red-star">*</span></label>
             <input type="file" id="photos" name="photos[]" accept="image/*" value="" multiple required>
         </div>
         </div>
         
-        <input type="submit" name="submit" value="Ajouter l'annonce" class="btn-submit">
+        <input type="submit" name="edit" value="Modifier l'annonce" class="btn-submit">
     </form>
 
 <?php
